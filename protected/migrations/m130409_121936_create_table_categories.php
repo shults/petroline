4348a7684a7db6444385aa180ba871c6f567bca4
Yaroslav Kotsur <yarikkotsur@gmail.com>
2013-04-09 18:25:02 +0300

    migrations product/product_images created

--- protected/migrations/m130409_121936_create_table_categories.php
+++ protected/migrations/m130409_121936_create_table_categories.php
@@ -7,13 +7,14 @@ class m130409_121936_create_table_categories extends CDbMigration
         $this->createTable('{{categories}}', array(
             'category_id' => 'SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
             'parent_category_id' => 'SMALLINT UNSIGNED NOT NULL DEFAULT 0',
-            'title' => 'VARCHAR(100)',
-			'url' => 'VARCHAR(100)', 
+            'title' => 'VARCHAR(100) NOT NULL',
+			'url' => 'VARCHAR(100) NOT NULL', 
             'status' => 'BOOLEAN NOT NULL DEFAULT 1',
             'filename' => 'VARCHAR(100)',
             'meta_title' => 'VARCHAR(100)',
             'meta_description' => 'TEXT',
-            'meta_keywords' => 'TEXT'
+            'meta_keywords' => 'TEXT',
+            'description' => 'TEXT',
         ));
     }
 
