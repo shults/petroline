99a73e742978eff89642bba6650985f825c8a736
Yaroslav Kotsur <yarikkotsur@gmail.com>
2013-04-09 17:44:41 +0300

    categories model changed

---
+++ protected/messages/ru/categories.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'Parent category' => 'Родительская категория',
+    'Active category' => 'Категория активна',
+    '-= No parent category =-' => '-= Нет родительской категории =-',
+    'Name' => 'Название',
+    'Image' => 'Изображение',
+    'Title' => 'Заголовок',
+    'Meta keyowrds' => 'Ключевые слова (meta)',
+    'Meta description' => 'Описание (meta)',
+    'Categories' => 'Категории',
+    'category' => 'категорию',
+    'categories' => 'категории'
+);
--- protected/models/Categories.php
+++ protected/models/Categories.php
@@ -3,7 +3,15 @@
 class Categories extends CActiveRecord
 {
 
-    public $adminNames = array('Категории', 'категорию', 'категории');
+    protected static $_adminNames;
+
+    public function getAdminNames()
+    {
+        if (self::$_adminNames === null) {
+            self::$_adminNames = array(self::t('Categories'), self::t('category'), self::t('categories'));
+        }
+        return self::$_adminNames;
+    }
 
     public function tableName()
     {
@@ -25,38 +33,12 @@ class Categories extends CActiveRecord
         return parent::model($className);
     }
 
-    public function beforeValidate()
-    {
-        return true;
-    }
-
-    /*public function beforeSave()
-    {
-        if ($image = CUploadedFile::getInstance(self::model(), 'image')) {
-            $extName = $image->getExtensionName();
-            $path = Yii::app()->getBasePath() . "/../uploads/categories/";
-            $filename = $this->url . "." . $extName;
-
-            if (!is_dir($path)) {
-                mkdir($path);
-                chmod($path, 0777);
-            }
-            if (is_file($image->getTempName())) {
-                if (rename($image->getTempName(), $path . $filename)) {
-                    chmod($path . $filename, 0777);
-                    $this->filename = $filename;
-                }
-            }
-        }
-        parent::beforeSave();
-    }*/
-
     public function rules()
     {
         return array(
-            array('url', 'unique'),
             array('title, url', 'required'),
-            array('url', 'url', 'pattern' => '/[a-z0-9-_\/]+/is'),
+            array('url', 'unique'),
+            array('url', 'url', 'pattern' => '/^[a-z0-9-_]+$/'),
             array('status, parent_category_id, filename, meta_title, meta_keywords, meta_description', 'safe'),
             array('filename', 'file', 'allowEmpty' => true, 'types' => 'jpg,jpeg,png,gif'),
         );
@@ -65,14 +47,14 @@ class Categories extends CActiveRecord
     public function attributeLabels()
     {
         return array(
-            'parent_category_id' => 'Родитель',
-            'status' => 'Категория активна',
-            'title' => 'Название',
+            'parent_category_id' => self::t('Parent category'),
+            'status' => self::t('Active category'),
+            'title' => self::t('Name'),
             'url' => 'URL',
-            'filename' => 'Изображение',
-            'meta_title' => 'Заголовок',
-            'meta_keywords' => 'Ключевые слова',
-            'meta_description' => 'Описание (meta)'
+            'filename' => self::t('image'),
+            'meta_title' => self::t('Title'),
+            'meta_keywords' => self::t('Meta keyowrds'),
+            'meta_description' => self::t('Meta description')
         );
     }
 
@@ -87,14 +69,27 @@ class Categories extends CActiveRecord
         );
     }
 
-    public function getParentTitleByParentId($parentId)
+    public function getParentTitle()
     {
-        return self::model()->find('category_id=:category_id', array(':category_id' => $parentId))->title;
+        if ($this->parent) {
+            return $this->parent->title;
+        }
+        return self::t('-= No parent category =-');
     }
 
     public function parent_category_idChoices()
     {
-        return CHtml::listData(self::model()->findAll(), 'category_id', 'title');
+        if ($this->getIsNewRecord()) {
+            return CHtml::listData(
+                            self::model()->findAll(
+                                    'parent_category_id=:parent_category_id', array(':parent_category_id' => 0)), 'category_id', 'title');
+        } else {
+            return CHtml::listData(self::model()->findAll(
+                                    'parent_category_id=:parent_category_id AND category_id!=:current_category_id', array(
+                                ':parent_category_id' => 0,
+                                ':current_category_id' => $this->category_id
+                            )), 'category_id', 'title');
+        }
     }
 
     public function adminSearch()
@@ -102,15 +97,15 @@ class Categories extends CActiveRecord
         return array(
             'columns' => array(
                 array(
-					'name' => 'image',
-					'value' => 'CHtml::image($data->getFileUrl("filename"), "", array("width" => 150));',
-					'type' => 'raw',
-				),
+                    'name' => 'image',
+                    'value' => 'CHtml::image($data->getFileUrl("filename"), "", array("width" => 150));',
+                    'type' => 'raw',
+                ),
                 'title',
                 'url',
                 array(
                     'name' => 'parent_category_id',
-                    'value' => '$data->parent_category_id == 0 ? "Нет" : $data->getParentTitleByParentId($data->parent_category_id);',
+                    'value' => '$data->getParentTitle();',
                 ),
             ),
         );
@@ -119,15 +114,21 @@ class Categories extends CActiveRecord
     public function relations()
     {
         return array(
-                //'parent' => array(self::HAS_ONE, 'Categories', array('page_id' => 'parent_page_id')),
-                //'children' => array(self::HAS_MANY, 'Categories', array('parent_page_id' => 'page_id'))
+            'parent' => array(self::HAS_ONE, 'Categories', array('category_id' => 'parent_category_id')),
+            'children' => array(self::HAS_MANY, 'Categories', array('parent_category_id' => 'category_id'))
         );
     }
-    
-    public function behaviors() {
-		return array(
-			'FileBehavior' => 'application.behaviors.FileBehavior'
-		);
-	}
+
+    public function behaviors()
+    {
+        return array(
+            'FileBehavior' => 'application.behaviors.FileBehavior'
+        );
+    }
+
+    public static function t($message, $params = null, $source = null, $language = null)
+    {
+        return Yii::t('categories', $message, $params, $source, $language);
+    }
 
 }
