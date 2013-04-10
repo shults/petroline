1f92265aac7cc1a9390481c1a2efd40ee89fe35f
Yaroslav Kotsur <yarikkotsur@gmail.com>
2013-04-10 18:18:15 +0300

    change form addded

---
+++ protected/modules/ycm/models/ChangePasswordForm.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ChangePasswordForm
+ *
+ * @author shults
+ */
+class ChangePasswordForm extends CFormModel
+{
+
+    public $password;
+    public $password_repeat;
+    public $old_password;
+
+    public function rules()
+    {
+        return array(
+            array('password, password_repeat, old_password', 'required'),
+            array('password', 'compare'),
+            array('password', 'length', 'allowEmpty' => false, 'min' => 6),
+            array('old_password', 'checkOldPassword')
+        );
+    }
+
+    public function checkOldPassword($attribute, $params)
+    {
+        $currentUser = Yii::app()->user->getModel();
+        if ($currentUser->password !== sha1($this->$attribute)) {
+            $this->addError($attribute, Yii::t('user', 'Old password is incorret.'));
+        }
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+            'password' => Yii::t('user', 'New password'),
+            'password_repeat' => Yii::t('user', 'New password confirm'),
+            'old_password' => Yii::t('user', 'Old password')
+        );
+    }
+
+}
