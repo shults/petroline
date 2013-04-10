e64c5fe085fe12c754f5a924cdf849e06bb85130
Yaroslav Kotsur <yarikkotsur@gmail.com>
2013-04-10 19:29:08 +0300

    config seed changed

--- protected/seeds/{{config}}.php
+++ protected/seeds/{{config}}.php
@@ -1,63 +1,29 @@
 <?php
 
 return array(
-	array(
-		'key' => 'company',
-		'title' => 'Название компании',
-		'value' => 'Компания'
-	),
-	array(
-		'key' => 'title',
-		'title' => 'Заголовок (Title)',
-		'value' => 'Компания'
-	),
-	array(
-		'key' => 'meta_keywords',
-		'title' => 'Ключевые слова (meta)',
-		'value' => 'keyword1, keyword2, keyword3'
-	),
-	array(
-		'key' => 'meta_description',
-		'title' => 'Описание (meta)',
-		'value' => 'Some description...'
-	),
-	array(
-		'key' => 'admin_email',
-		'title' => 'E-Mail администратора',
-		'value' => 'admin@exaple.com'
-	),
-	array(
-		'key' => 'contact_meta_title',
-		'title' => 'Заголовок на странице контактов',
-		'value' => 'Контакты'
-	),
-	array(
-		'key' => 'contact_meta_keywords',
-		'title' => 'Ключевые слова на странице контактов',
-		'value' => 'Контакты'
-	),
-	array(
-		'key' => 'contact_meta_description',
-		'title' => 'Мета описание на странице контактов',
-		'value' => 'Контакты'
-	),
-	array(
-		'key' => 'contact_text',
-		'title' => 'Текст на странице контактов',
-		'value' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla dolor, placerat eget porttitor vel, vehicula pellentesque dui. Pellentesque facilisis massa ut velit euismod quis gravida arcu tempor. Aenean iaculis bibendum arcu, at tincidunt nibh suscipit in. Vestibulum ullamcorper nisl non nisl eleifend et elementum augue vehicula. Aenean feugiat libero vel enim fermentum ornare. Nulla sed dolor sit amet nisl lacinia vestibulum eget nec ante. Aliquam commodo suscipit ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas pulvinar congue vehicula. In porta lobortis mattis. Integer a urna orci, posuere posuere urna. Cras odio justo, bibendum a consectetur ut, porta in nunc. Mauris nec nulla odio, ut fringilla enim. </p>
-			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla dolor, placerat eget porttitor vel, vehicula pellentesque dui. Pellentesque facilisis massa ut velit euismod quis gravida arcu tempor. Aenean iaculis bibendum arcu, at tincidunt nibh suscipit in. Vestibulum ullamcorper nisl non nisl eleifend et elementum augue vehicula. Aenean feugiat libero vel enim fermentum ornare. Nulla sed dolor sit amet nisl lacinia vestibulum eget nec ante. Aliquam commodo suscipit ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas pulvinar congue vehicula. In porta lobortis mattis. Integer a urna orci, posuere posuere urna. Cras odio justo, bibendum a consectetur ut, porta in nunc. Mauris nec nulla odio, ut fringilla enim. </p>',
-		'widget' => 'wysiwyg'
-	),
-	array(
-		'key' => 'home_text',
-		'title' => 'Текст на главной странице',
-		'value' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla dolor, placerat eget porttitor vel, vehicula pellentesque dui. Pellentesque facilisis massa ut velit euismod quis gravida arcu tempor. Aenean iaculis bibendum arcu, at tincidunt nibh suscipit in. Vestibulum ullamcorper nisl non nisl eleifend et elementum augue vehicula. Aenean feugiat libero vel enim fermentum ornare. Nulla sed dolor sit amet nisl lacinia vestibulum eget nec ante. Aliquam commodo suscipit ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas pulvinar congue vehicula. In porta lobortis mattis. Integer a urna orci, posuere posuere urna. Cras odio justo, bibendum a consectetur ut, porta in nunc. Mauris nec nulla odio, ut fringilla enim. </p>
-			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla dolor, placerat eget porttitor vel, vehicula pellentesque dui. Pellentesque facilisis massa ut velit euismod quis gravida arcu tempor. Aenean iaculis bibendum arcu, at tincidunt nibh suscipit in. Vestibulum ullamcorper nisl non nisl eleifend et elementum augue vehicula. Aenean feugiat libero vel enim fermentum ornare. Nulla sed dolor sit amet nisl lacinia vestibulum eget nec ante. Aliquam commodo suscipit ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas pulvinar congue vehicula. In porta lobortis mattis. Integer a urna orci, posuere posuere urna. Cras odio justo, bibendum a consectetur ut, porta in nunc. Mauris nec nulla odio, ut fringilla enim. </p>',
-		'widget' => 'wysiwyg'
-	),
-	array(
-		'key' => 'msg_send_success',
-		'title' => 'Сообщение отправлено удачно',
-		'value' => 'Сообщение отправлено удачно. Ждите ответа.'
-	)
+    array(
+        'key' => 'company',
+        'title' => 'Название компании',
+        'value' => 'Компания'
+    ),
+    array(
+        'key' => 'title',
+        'title' => 'Заголовок (Title)',
+        'value' => 'Компания'
+    ),
+    array(
+        'key' => 'meta_keywords',
+        'title' => 'Ключевые слова (meta)',
+        'value' => 'keyword1, keyword2, keyword3'
+    ),
+    array(
+        'key' => 'meta_description',
+        'title' => 'Описание (meta)',
+        'value' => 'Some description...'
+    ),
+    array(
+        'key' => 'admin_email',
+        'title' => 'E-Mail администратора',
+        'value' => 'admin@exaple.com'
+    ),
 );
