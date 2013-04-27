0be27b13ee464aad8105e8a63f23e42836ff53fc
Yaroslav Kotsur <yarikkotsur@gmail.com>
2013-04-27 19:09:33 +0300

    field flag added

--- protected/migrations/m130219_123122_create_table_languages.php
+++ protected/migrations/m130219_123122_create_table_languages.php
@@ -9,6 +9,7 @@ class m130219_123122_create_table_languages extends CDbMigration
             'language_id' => 'SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
             'code' => 'CHAR(2) NOT NULL',
             'title' => 'VARCHAR(100) NOT NULL',
+            'flag' => 'VARCHAR(10) NOT NULL',
             'default' => 'BOOLEAN NOT NULL DEFAULT 0',
             'enabled' => 'BOOLEAN NOT NULL DEFAULT 1',
             'deleted' => 'BOOLEAN NOT NULL DEFAULT 0'
--- protected/seeds/{{languages}}.php
+++ protected/seeds/{{languages}}.php
@@ -5,14 +5,16 @@ return array(
         'language_id' => 1,
         'code' => 'ru',
         'title' => 'Русский',
+        'flag' => 'ru.png',
         'default' => 1,
         'enabled' => 1,
-        'deleted' => 0
+        'deleted' => 0,
     ),
     array(
         'language_id' => 2,
         'code' => 'uk',
         'title' => 'Українська',
+        'flag' => 'ua.png',
         'default' => 0,
         'enabled' => 1,
         'deleted' => 0
@@ -21,8 +23,9 @@ return array(
         'language_id' => 3,
         'code' => 'en',
         'title' => 'English',
+        'flag' => 'gb.png',
         'default' => 0,
-        'enabled' => 0,
+        'enabled' => 1,
         'deleted' => 0
     )
 );
