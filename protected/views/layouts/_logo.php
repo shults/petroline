4782c8f9bc8c757cbf6b4a9287aae2f68aba4722
Yaroslav Kotsur <yarikkotsur@gmail.com>
2013-05-09 13:10:20 +0300

    logo fix

---
+++ protected/views/layouts/_logo.php
@@ -0,0 +1,5 @@
+<div class="logo" style="height: 84px; ">
+    <a href="<?php echo CHtml::normalizeUrl(array('site/index')) ?>">
+        <img src="/images/logo_<?php echo Yii::app()->lang->code ?>.gif" border="0" />
+    </a>
+</div>
